6b9ba3d1fbf6b47b684c42ffabdcc9ec768a5afc
kaydeeargh <kdr220721@gmail.com>
2017-09-25 20:45:21 -0500

    Add files via upload
    Master - Connects to requested server, tests domain and requests credentials from user based on domain.
    
    LowDiskSpace - Returns disk space on selected drive from server and runs cleanmgr, clears old windows update folders, old profiles, etc.
    
    Healthservice - Tests to see if the service is running, stops and restarts the service.
    
    FailedToConnect - Work in progress. Is meant to test connection to servers from list and return which servers are not responding.

---
+++ FailedToConnect.ps1
@@ -0,0 +1,38 @@
+﻿$SecSep = '--------------------------------'
+
+##Variables
+$successarray = @()
+$failarray = @()
+$report =  'C:\Users\kdd0921\Documents\BMC_Reports\Report.csv'
+
+$a = Import-Csv $report | ForEach-Object {
+if ($_.Notes -match 'Microsoft.SystemCenter.AgentWatchersGroup - The computer |  was not accessible.')
+    {
+    $_.Notes -replace 'Microsoft.SystemCenter.AgentWatchersGroup - The computer', '' -replace 'was not accessible.', ''
+
+    }
+}
+
+$b = $null
+
+
+
+write-output $a
+
+$SecSep
+
+foreach ($item in $a)
+{
+    $t = Test-Connection -Quiet -computername $item -ErrorAction SilentlyContinue -ErrorVariable ProcessError
+    if ($t -eq $true)
+    {
+        Write-Output "$item Success!"
+        $successarray += ,$item
+    }
+    else
+    {
+        $failarray += ,$item
+    }
+}
+
+$SecSep
---
+++ Master.ps1
@@ -0,0 +1,81 @@
+﻿################################
+# Global Variables & Functions #
+################################
+
+$SecSep = '--------------------------------'
+$ServerName =
+$OptionSelected =
+$Spacing = Write-Host `n
+$domain = 
+$cred = $null
+
+Do {
+
+$user = "{0}\{1}" -f $env:USERDOMAIN, $env:USERNAME
+
+Write-Host '#########################################################' `n
+Write-Host "Welcome to KDR's Script! What server are you working on?" `n
+Write-Host '#########################################################' `n
+
+Function Domain {
+    try{
+        Get-WmiObject win32_computersystem -ComputerName $ServerName | Select -ExpandProperty "Domain" > $null;
+    } catch {
+        $Global:user = "{0}\{1}" -f "ReynoldsPKG", $env:USERNAME
+    }
+    $Global:cred = Get-Credential -Credential $user
+    $Global:domain = Get-WmiObject win32_computersystem -ComputerName $ServerName -Credential $cred | Select -ExpandProperty "Domain"
+    Write-Host "Domain: $domain"
+    $Spacing
+
+}
+Function SrvrName {
+$Global:ServerName = (Read-Host -Prompt 'Server Name').ToUpper()
+
+}
+Function OpSelect {
+$Global:OptionSelected = Read-Host -Prompt 'Please select an option'
+
+}
+
+#-------------------#
+# Options to Select #
+#-------------------#
+
+SrvrName
+Domain
+$Spacing
+Write-Host 'Please select from the following options:' `n
+$SecSep
+Write-Host '1.Health Service Heartbeat Failure' `n
+Write-Host '2.Failed to Connect to Computer' `n
+Write-Host '3.Logical Disk Free Space is low' `n
+Write-Host '10. Exit and Close Console' `n
+$SecSep
+Write-Host `n
+OpSelect
+
+#-------------------#
+# Calls to Scripts  #
+#-------------------#
+Set-ExecutionPolicy Bypass -Scope Process
+
+If ($OptionSelected -eq 1){
+cd C:\PS_Scripts
+& .\Healthservice.PS1
+}
+
+If ($OptionSelected -eq 3){
+cd C:\PS_Scripts
+& .\LowDiskSpace.ps1
+}
+
+
+
+$SecSep
+
+Pause
+
+Clear-Host
+} Until ($OptionSelected -eq 10)
+Exit
